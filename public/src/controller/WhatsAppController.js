3db14af213dfb311caa9a34fd6f1ae89d9e7d6d8
Luiz Moura <luizmoura2@hotmil.com>
2020-07-15 22:38:53 -0300

    152_W41 - Sending photo as an attachment

--- public/src/controller/WhatsAppController.js
+++ public/src/controller/WhatsAppController.js
@@ -382,10 +382,46 @@ initEvents(){
         });
 
         this.el.btnSendPicture.on('click', e=>{
-            console.table('aqui eu');
-            console.info('aqui eu');
-            console.log(this.el.pictureCamera.src);
-            this._camera.stop();
+            
+            this.el.btnSendPicture.disabled = true;
+
+            let regex = /^data:(.+);base64,(.*)$/;
+            let result = this.el.pictureCamera.src.match(regex);
+            let mimeType = result[1];
+            let ext = mimeType.split('/')[1];
+            let fileName = `camera_${Date.now()}.${ext}`;
+
+            let picture = new Image();
+            picture.src = this.el.pictureCamera.src;
+            picture.on('load', e=>{
+                let canvas = document.createElement('canvas');
+                let context = canvas.getContext('2d');
+                canvas.width = picture.width;
+                canvas.height = picture.height;
+                context.translate(picture.width, 0);
+                context.scale(-1, 1);
+                context.drawImage(picture, 0, 0, canvas.width, canvas.height);
+
+                fetch(canvas.toDataURL(mimeType))
+                    .then(res=>{
+                        return res.arrayBuffer()
+                    }).then(buffer=>{
+                        return new File([buffer], fileName, {type: mimeType});
+                    }).then(file=>{
+                        Message.sendImage(this._contactActive.chatId, this._user.email, file);
+                        this.el.btnSendPicture.disabled = false;
+
+                        this.closeAllMainPanel();
+                        this._camera.stop();
+                        this.el.btnReshootPanelCamera.hide();
+                        this.el.pictureCamera.hide();
+                        this.el.videoCamera.show();
+                        this.el.containerSendPicture.hide();
+                        this.el.containerTakePicture.show();
+                        this.el.panelMessagesContainer.show();
+
+                    });
+            });
         })
 
         /** Eventos para anexar contatos */
